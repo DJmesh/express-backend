c4226c23bbcfd19969abf10e0a7fc75a18ff23b7
Eduardo Prestes <eduardo.prestes@facens.br>
2025-03-16 18:31:50 -0300

    refactor: ajustes nos models de index e user

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,35 @@
+// src/models/user.js
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    nome: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    senha: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.ENUM('user','admin'), allowNull: false, defaultValue: 'user' },
+    nome: { 
+      type: DataTypes.STRING, 
+      allowNull: false 
+    },
+    email: { 
+      type: DataTypes.STRING, 
+      allowNull: false, 
+      unique: true 
+    },
+    senha: { 
+      type: DataTypes.STRING, 
+      allowNull: false 
+    },
+    role: { 
+      type: DataTypes.ENUM('user','admin'), 
+      allowNull: false, 
+      defaultValue: 'user' 
+    },
   }, {
     tableName: 'users',
     timestamps: true
   });
+
+  // Se desejar associar o usuário às ocorrências:
+  User.associate = (models) => {
+    User.hasMany(models.Incident, { foreignKey: 'userId', as: 'incidents' });
+  };
 
   return User;
 };
